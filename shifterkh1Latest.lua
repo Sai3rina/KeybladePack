d167bcef40497a686401d47e885233955bc6a90c
Sai3rina <sai3rina@gmx.de>
2025-12-14 00:10:50 +0100

    Formkey Shapeshifter working again

---
+++ shifterkh1Latest.lua
@@ -0,0 +1,80 @@
+LUAGUI_NAME = "WeaponpackKH1"
+LUAGUI_AUTH = "Kité"
+LUAGUI_DESC = "World-based costume shifter"
+
+--- Game Version Check ---
+epiccheck = 0x585B61
+stmcheck = epiccheck + 0x2F8
+stmjpcheck = epiccheck + 0x2A8
+
+function _OnInit()
+    if ENGINE_TYPE == "BACKEND" then
+        IsEpicGame = 0
+        IsSteamGame = 0
+        IsSteamJPGame = 0
+    end
+    print('Weaponpack')
+end
+
+function _OnFrame()
+    -- Version Detection
+    if ENGINE_TYPE == "BACKEND" then
+        if ReadLong(epiccheck) == 0x7265737563697065 and IsEpicGame == 0 then
+            IsEpicGame = 1
+            Now = 0x0716DF8
+            Obj0Pointer = 0x2A24A70
+            print('Epic Version')
+        end
+        
+        if ReadLong(stmcheck) == 0x7265737563697065 and IsSteamGame == 0 then
+            IsSteamGame = 1
+            Now = 0x0717008
+            Obj0Pointer = 0x2A25030
+            print('Steam Global Version')
+        end
+        
+        if ReadLong(stmjpcheck) == 0x7265737563697065 and IsSteamJPGame == 0 then
+            IsSteamJPGame = 1
+            Now = 0x0716008
+            Obj0Pointer = 0x2A23FB0
+            print('Steam JP Version')
+        end
+    end
+    
+    -- Only run if version detected
+    if IsEpicGame == 1 or IsSteamGame == 1 or IsSteamJPGame == 1 then
+        World = ReadByte(Now + 0x00)
+        Obj0 = ReadLong(Obj0Pointer)
+        CostumeSwap()
+    end
+end
+
+function CostumeSwap()
+    if World == 0x07 then --Agrabah
+        WriteString(Obj0+0x1BD0,'W_EX020_Aladin\0', true)
+    elseif World == 0x10 then --Port Royal
+        WriteString(Obj0+0x1BD0,'W_EX020_Jack\0', true)
+    elseif World == 0x02 then --Twilight Town
+        WriteString(Obj0+0x1BD0,'W_EX020_Twilight\0', true)
+    elseif World == 0x04 then --Hollow bastion
+        WriteString(Obj0+0x1BD0,'W_EX020_Hollow\0', true)
+    elseif World == 0x05 then --Beasts Castle
+        WriteString(Obj0+0x1BD0,'W_EX020_Beast\0', true)
+    elseif World == 0x06 then --olymp
+        WriteString(Obj0+0x1BD0,'W_EX020_Herc\0', true)
+    elseif World == 0x08 then --Mulan
+        WriteString(Obj0+0x1BD0,'W_EX020_Mulan\0', true)
+    elseif World == 0x09 then --100acer
+        WriteString(Obj0+0x1BD0,'W_EX020_puh\0', true)
+    elseif World == 0x0a then --Pride Lands
+        WriteString(Obj0+0x1BD0,'W_EX020_Lion\0', true)
+    elseif World == 0x0b then --Atlantica
+        WriteString(Obj0+0x1BD0,'W_EX020_atlantica\0', true)
+    elseif World == 0x0c then --Disney Castle
+        WriteString(Obj0+0x1BD0,'W_EX020_Mickey\0', true)
+    elseif World == 0x12 then --Final world
+        WriteString(Obj0+0x1BD0,'W_EX020_Final\0', true)
+    else --Revert costume changes
+        WriteString(Obj0+0x1BD0,'W_EX010_20\0', true)
+    end
+end
